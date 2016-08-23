93ec00f838a646f55cdaa6be59ff36e602078a10
Jimeno0 <carlospj@icloud.com>
2016-08-23 12:53:13 +0200

    adding md button sheet

--- users/UserController.js
+++ users/UserController.js
@@ -1,14 +1,14 @@
 angular
   .module('users')
-  .controller('UserController',['userService',UserController]);
+  .controller('UserController',['userService','$mdBottomSheet', UserController]);
 
-function UserController(userService){
+function UserController(userService, $mdBottomSheet){
   var self=this;
 
     self.selected     = null;
     self.users        = [ ];
     self.selectUser   = selectUser;
-
+    self.makeContact         = makeContact;
     userService
           .loadAllUsers()
           .then( function( users ) {
@@ -19,6 +19,36 @@ function UserController(userService){
 
     function selectUser ( user ) {
       self.selected = angular.isNumber(user) ? $scope.users[user] : user;
+    }
+
+    function makeContact (selectedUser) {
+      console.log('share clicked!');
+      $mdBottomSheet.show({
+          controller    : ContactSheetController,
+          controllerAs  : "vm",
+          templateUrl   : 'users/contactSheet.html',
+          parent        : angular.element(document.querySelector('#content'))
+      
+        });
+
+        /**
+         * User ContactSheet controller
+         */
+        function ContactSheetController() {
+          this.user = selectedUser;
+          this.items = [
+            { name: 'Phone'       , icon: 'phone'       , icon_url: 'svg/phone.svg'},
+            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'svg/twitter.svg'},
+            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'svg/google_plus.svg'},
+            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'svg/hangouts.svg'}
+          ];
+          this.contactUser = function(action) {
+            // The actually contact process has not been implemented...
+            // so just hide the bottomSheet
+
+            $mdBottomSheet.hide();
+            };
+        }
     }
 
 }
